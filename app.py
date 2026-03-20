88c3ff9c215eabb710a5f4a10715c3983a85a3d4
mariew999 <pinedaprecious52@gmail.com>
2026-03-20 23:03:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from views import views
+import os
 
 app = Flask(__name__)
 app.secret_key = 'hatdogka'
-app.register_blueprint(views, url_prefix='/')
+app.register_blueprint(views)
 
+# This is needed for Render
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
